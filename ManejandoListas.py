a0fb8f352ef37918be65de57631a32c97560492d
Robert Moreira <robertmoreiracenteno@hotmail.com>
2019-06-26 20:55:52 -0500

    Manejo de listas

---
+++ ManejandoListas.py
@@ -0,0 +1,7 @@
+#Manejando listas
+L=[True,"Una Lista",[5,7]]
+print(L[0])
+print(L[1])
+print(L[2])
+print(L[2][0]) #Imprime un elemento de una lista interna
+print(L[3]) #Error pues no existe ese elemento
