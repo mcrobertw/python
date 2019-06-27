b313348edc97ec97819c7d9f26ec0041cc1271cb
Robert Moreira <robertmoreiracenteno@hotmail.com>
2019-06-26 21:02:50 -0500

    Manejo de tuplas

--- ManejandoTuplas.py
+++ ManejandoTuplas.py
@@ -1,9 +1,8 @@
 #Manejando listas
-L=[True,"Una Lista",[5,7]]
+L=(True,"Una Lista",[5,7])
 print(L[0])
 print(L[1])
-L[1]="Otra Lista"
+L[1]="Otra Lista" #Esta línea en una tupla arroja error
 print(L[1])
 print(L[2])
 print(L[2][0]) #Imprime un elemento de una lista interna
-#print(L[3]) #Error pues no existe ese elemento
